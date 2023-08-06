894cce4e8050689f6e69ac0ea65a87f550230fd3
MedinaJeser <jesermedina570@gmail.com>
2023-08-06 03:08:10 -0600

    [chore]: redis

--- Practica1/FastAPI/redis_client/connection.py
+++ Practica1/FastAPI/redis_client/connection.py
@@ -5,9 +5,9 @@ from os import getenv
 
 try:
     redis_client = Redis(
-        host='us1-lasting-loon-38131.upstash.io',
+        host='',
         port=38131,
-        password='1875241040494314b87e11bb6933ea3a'
+        password=''
     )
 
     print("Connected to Redis")
