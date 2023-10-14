bbdb05aa9ef4b60df218f7093793e9c78ee1ac20
akamariano <marianitorac2002@gmail.com>
2023-10-14 00:52:24 -0600

    [add]:Grafana Graphs

--- docs/index.js
+++ docs/index.js
@@ -18,3 +18,10 @@ encenderButton.addEventListener('click', function () {
     toggleButtonText(this); 
     toggleButtonColor(this); 
 });
+var verGraficasButton = document.getElementById('verGraficas');
+
+   
+    verGraficasButton.addEventListener('click', function() {
+       
+        window.open('https://arqui2database.grafana.net/d/f807ac0a-d5be-4390-9048-e9bc4cef28b4/arqui2?orgId=1&from=1697093219717&to=1697266019717', '_blank');
+    });
